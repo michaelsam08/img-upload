2b554c71090b98a0792a50c84e7bc716151938b7
michael.samara <michael.samara@versasuite.com>
2019-04-09 08:49:34 -0500

    file upload commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    selectedFile: null
+  }
+  fileSelectedHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0]
+    })
+    console.log(event.target.files[0]);
+  }
+  // fileUploadHandler =()=>{
+  //   const fd = new FormData();
+  //   fd.append('image', this.state.selectedFile,this.state.selectedFile.name)
+  //   axios.post('https://', fd)
+
+  // }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input 
+        style={{display: 'none'}} 
+        type="file" 
+        onChange={this.fileSelectedHandler}
+        ref={fileInput=> this.fileInput= fileInput} />
+
+        <button onClick={()=> this.fileInput.click()}>Pick File</button>
+        <button onClick={this.fileUploadHandler}>Upload</button>
       </div>
     );
   }
